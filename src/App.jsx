d248d40459a3adb3ddf9db1885ba5e9cad59bb4a
Alexandr Menshov <mensov02@gmail.com>
2025-09-13 20:39:08 +0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [isRolled, setIsRolled] = useState(false)
 
   function shuffleArray(array) {
-  const shuffled = [...array]; // создаем копию массива
+  const shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
