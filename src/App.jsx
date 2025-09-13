f02745f8902fbe0246a92221492a570d26219348
Alexandr Menshov <mensov02@gmail.com>
2025-09-13 15:42:04 +0400

    initialCommit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [participants, setParticipants] = useState(['Эмерскибиби', 'Эмерскигиги', 'Эмерскидиди'])
+  const [isRolled, setIsRolled] = useState(false)
+
+  function shuffleArray(array) {
+  const shuffled = [...array]; // создаем копию массива
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+  return (
+    <div className='app'>
+      <h2 className='heading'>Определение очереди хапульки</h2>
+      <button onClick={() => {
+        setParticipants(shuffleArray(participants))
+        setIsRolled(true)
+      }}>Roll</button>
+      <ul>
+        {isRolled && 
+        participants.map((item, index) => {
+          return <li>{`${index + 1}. ${item}`}</li>
+        })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
